128defd0ca98ceefa812864c347816452b959a0d
JoKoBlu96 <153492211+JoKoBlu96@users.noreply.github.com>
2025-06-18 09:59:56 +0200

    Add test.py from local project to haven repository

---
+++ test.py
@@ -0,0 +1,24 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("MyServer")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+
+    """Add two numbers."""
+
+    return a + b
+
+@mcp.resource("greeting://{name}")
+def greet(name: str) -> str:
+
+    return f"Hello, {name}!"
+
+@mcp.prompt()
+def review_code(code: str) -> str:
+
+    return f"Please review:\n{code}"
+
+if __name__ == "__main__":
+
+    mcp.serve_stdio()
